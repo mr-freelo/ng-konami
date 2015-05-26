ed70b2b357bce3e4ed76bd9fc854fc2d97845237
Francois Dabiel <dabielf@gmail.com>
2015-05-26 22:16:57 +0200

    initial push

---
+++ README.md
@@ -0,0 +1,4 @@
+ng-konami
+==============
+
+An Angular service to add a Konami code to your App, quickly.
---
+++ konami.js
@@ -0,0 +1,33 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('konami', [])
+    .factory('konamiCode', konamiCode);
+
+  function konamiCode() {
+
+    return {
+      set: set
+    }
+
+    function set(callback) {
+      var seq = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65],
+        i = 0;
+
+      document.body.onkeydown = function(e) {
+        if (e.keyCode === seq[i]) {
+          i++;
+          if (i > 9) {
+            i = 0;
+            fn.call(this);
+          }
+        } else {
+          i = 0;
+        }
+      }
+    }
+
+  }
+
+}());
